1db5e19a62e020473e6782dc33252fd174236e60
Rune Lambert <runelambert@LaptopVanNeomie.vivesnet.be>
2025-09-30 11:21:37 +0200

    creation of TripPlanner

---
+++ TripPlanner/TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Rune Lambert on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
