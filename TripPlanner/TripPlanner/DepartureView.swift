24b7a2bd813f9e75bf6fa8bb7f24b0a11a40fc5d
Rune Lambert <runelambert@LaptopVanNeomie.vivesnet.be>
2025-09-30 13:00:09 +0200

    first tab in tabview completed tripPlanner

---
+++ TripPlanner/TripPlanner/DepartureView.swift
@@ -0,0 +1,18 @@
+//
+//  DepartureView.swift
+//  TripPlanner
+//
+//  Created by Rune Lambert on 30/09/2025.
+//
+
+import SwiftUI
+
+struct DepartureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepartureView()
+}
