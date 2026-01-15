6fa56bd1f4a0d52e09cfba2e904ccc2fa91edbcc
lambertR <146202170+LambertRune@users.noreply.github.com>
2026-01-15 16:21:55 +0100

    fix

---
+++ Practice/VivesPlus/VivesPlus/VivesPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VivesPlusApp.swift
+//  VivesPlus
+//
+//  Created by rune on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct VivesPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Practice/VivesPlus/VivesPlusTests/VivesPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  VivesPlusTests.swift
+//  VivesPlusTests
+//
+//  Created by rune on 15/01/2026.
+//
+
+import Testing
+@testable import VivesPlus
+
+struct VivesPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
